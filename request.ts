2f59e870d4e2e9c54bf9c9295db416276cc55553
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-08-19 11:12:03 +0900

    feat(request): add types for http methods

---
+++ request.ts
@@ -0,0 +1,20 @@
+/** HTTP request method. */
+export type Method =
+  /** RFC 9110, 9.3.1 */
+  | "GET"
+  /** RFC 9110, 9.3.2 */
+  | "HEAD"
+  /** RFC 9110, 9.3.3 */
+  | "POST"
+  /** RFC 9110, 9.3.4 */
+  | "PUT"
+  /** RFC 9110, 9.3.5 */
+  | "DELETE"
+  /** RFC 9110, 9.3.6 */
+  | "CONNECT"
+  /** RFC 9110, 9.3.7 */
+  | "OPTIONS"
+  /** RFC 9110, 9.3.8 */
+  | "TRACE"
+  /** RFC 5789 */
+  | "PATCH";
