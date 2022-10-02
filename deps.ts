6e71b17d935a32a9b4b44a9858921e5512737f6b
TomokiMiyauci <development.operation6.6.6@gmail.com>
2022-10-02 13:06:45 +0900

    chore(deps): up version

--- deps.ts
+++ deps.ts
@@ -6,5 +6,5 @@ export { isTruthy } from "https://deno.land/x/isx@1.0.0-beta.21/mod.ts";
 export {
   toLowerCase,
   trim,
-} from "https://deno.land/x/prelude_js@1.0.0-beta.1/string.ts";
+} from "https://deno.land/x/prelude_js@1.0.0-beta.2/mod.ts";
 export { default as db } from "https://deno.land/x/http_header_db@1.0.0-beta.1/db.json" assert { type: "json" };
