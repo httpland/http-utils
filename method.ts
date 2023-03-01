5e31439fdeaf4ea5a25bee094cebacc7552a45b9
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-01 21:51:30 +0900

    feat(method): add http method enum

---
+++ method.ts
@@ -0,0 +1,27 @@
+// Copyright 2022-latest the httpland authors. All rights reserved. MIT license.
+// This module is browser compatible.
+
+/** Standard HTTP methods. */
+export const enum Method {
+  /** RFC 9110, 9.3.1 */
+  Get = "GET",
+  /** RFC 9110, 9.3.2 */
+  Head = "HEAD",
+  /** RFC 9110, 9.3.3 */
+  Post = "POST",
+  /** RFC 9110, 9.3.4 */
+  Put = "PUT",
+  /** RFC 9110, 9.3.5 */
+  Delete = "DELETE",
+  /** RFC 9110, 9.3.6 */
+  Connect = "CONNECT",
+  /** RFC 9110, 9.3.7 */
+  Options = "OPTIONS",
+  /** RFC 9110, 9.3.8 */
+  Trace = "TRACE",
+  /** RFC 5789 */
+  Patch = "PATCH",
+}
+
+/** HTTP request method. */
+export type HttpMethod = Uppercase<keyof typeof Method>;
--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 // Copyright 2022-latest the httpland authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
-export { equalsRequest, type HttpMethod, isRequest } from "./requests.ts";
+export { equalsRequest, isRequest } from "./requests.ts";
 export { type HttpHandler } from "./handlers.ts";
 export {
   equalsHeaders,
@@ -12,3 +12,4 @@ export {
   parseFieldValue,
 } from "./headers.ts";
 export { equalsResponse, isResponse, safeResponse } from "./responses.ts";
+export { type HttpMethod, Method } from "./method.ts";
