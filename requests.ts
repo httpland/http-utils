6b45eb8de2f46a02dfb6b2f4cd61270d5156873a
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-01 21:51:44 +0900

    refactor(requests): remove duplicate

--- requests.ts
+++ requests.ts
@@ -2,26 +2,6 @@
 // This module is browser compatible.
 
 import { equalsHeaders } from "./headers.ts";
-/** HTTP request method. */
-export type HttpMethod =
-  /** RFC 9110, 9.3.1 */
-  | "GET"
-  /** RFC 9110, 9.3.2 */
-  | "HEAD"
-  /** RFC 9110, 9.3.3 */
-  | "POST"
-  /** RFC 9110, 9.3.4 */
-  | "PUT"
-  /** RFC 9110, 9.3.5 */
-  | "DELETE"
-  /** RFC 9110, 9.3.6 */
-  | "CONNECT"
-  /** RFC 9110, 9.3.7 */
-  | "OPTIONS"
-  /** RFC 9110, 9.3.8 */
-  | "TRACE"
-  /** RFC 5789 */
-  | "PATCH";
 
 /** Check two `Request` fields equality.
  *
