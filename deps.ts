f3855c4268fbb5a930c2b56f0a8468909e687550
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-04-02 00:10:11 +0900

    chore(deps): fix typo

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-latest the httpland authors. All rights reserved. MIT license.
+// Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
 export { isNull } from "https://deno.land/x/isx@1.1.1/is_null.ts";
