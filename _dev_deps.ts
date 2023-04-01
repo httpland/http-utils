32003d4172790596a4e459a36ca69de1fe5f4680
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-04-02 00:06:51 +0900

    chore: update deps

--- _dev_deps.ts
+++ _dev_deps.ts
@@ -1,11 +1,11 @@
-export { describe, it } from "https://deno.land/std@0.178.0/testing/bdd.ts";
+export { describe, it } from "https://deno.land/std@0.181.0/testing/bdd.ts";
 export {
   assert,
   assertEquals,
   assertFalse,
   assertRejects,
   assertThrows,
-} from "https://deno.land/std@0.178.0/testing/asserts.ts";
+} from "https://deno.land/std@0.181.0/testing/asserts.ts";
 
 // deno-lint-ignore no-explicit-any
 export type Fn<F extends (...args: any) => any> = [
