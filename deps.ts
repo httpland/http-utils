404620ef041b560e348129b838f5d474aa6caa8e
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-04-01 23:36:03 +0900

    feat(header): remove irrelevant modules

--- deps.ts
+++ deps.ts
@@ -1,6 +1,4 @@
 // Copyright 2022-latest the httpland authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
-export { toLowerCase } from "https://deno.land/x/prelude_js@1.0.0/to_lower_case.ts";
 export { isNull } from "https://deno.land/x/isx@1.1.1/is_null.ts";
-export { default as db } from "https://deno.land/x/http_header_db@1.0.0-beta.1/db.json" assert { type: "json" };
